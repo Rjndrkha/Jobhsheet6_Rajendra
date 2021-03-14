86846fcd5c0a74f8a1d062a85557474c0887b249
ArionoS <arionoseptian0802@gmail.com>
2021-03-14 14:25:33 +0700

    1

--- database/migrations/2021_03_14_072322_create_posts_table.php
+++ database/migrations/2021_03_14_072322_create_posts_table.php
@@ -12,12 +12,12 @@ class CreatePostsTable extends Migration
  public function up()
  {
  Schema::create('posts', function (Blueprint $table) {
-    $table->id();
-    $table->string('title', 100)->index();
-    $table->string('slug', 100)->index();
-    $table->text('content');
-    $table->boolean('draft')->default(false);
-    $table->timestamps();
+$table->id();
+ $table->string('title', 100)->index();
+ $table->string('slug', 100)->index();
+ $table->text('content');
+ $table->boolean('draft')->default(false);
+ $table->timestamps();
  });
  }
  /**
