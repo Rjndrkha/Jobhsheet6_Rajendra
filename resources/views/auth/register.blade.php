6c7ac4648715a179979d1050ba38bed797c9eaaf
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-03-19 14:46:51 +0700

    Change login Username

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,7 +31,7 @@
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
-                                @error('email')
+                                @error('username')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
